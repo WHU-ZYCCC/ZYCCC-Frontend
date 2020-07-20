d34bd2296bd0decff831678f73cb55da062ca558
A-Salty-Fish <1309032417@qq.com>
2020-07-20 17:56:26 +0800

    校对手机响应式

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,27 +1,32 @@
 <template>
     <div class="MainDiv">
-      <form class="loginForm">
-        <el-row></el-row>
-        <el-row><h2>登录</h2></el-row>
-        <el-row><p>使用学号与密码</p></el-row>
-        <el-row></el-row>
-        <el-row>
-          <el-col :span="4"><div>&nbsp;</div></el-col>
-          <el-col :span="16"><el-input placeholder="请输入学号"></el-input></el-col>
-          <el-col :span="4"></el-col>
-        </el-row>
-        <el-row></el-row>
-        <el-row></el-row>
-        <el-row>
-          <el-col :span="4"><div>&nbsp;</div></el-col>
-          <el-col :span="16"><el-input placeholder="请输入密码" show-password></el-input></el-col>
-          <el-col :span="4"></el-col>
+      <el-row>
+        <el-col :sm="15" :xs="2"><div>&nbsp;</div></el-col>
+        <el-col :sm="6" :xs="21"><form class="loginForm">
+          <el-row></el-row>
+          <el-row><h2>登录</h2></el-row>
+          <el-row><p>使用学号与密码</p></el-row>
+          <el-row></el-row>
+          <el-row>
+            <el-col :span="4"><div>&nbsp;</div></el-col>
+            <el-col :span="16"><el-input placeholder="请输入学号"></el-input></el-col>
+            <el-col :span="4"></el-col>
           </el-row>
-        <el-row></el-row>
-        <el-row><el-button type="primary">登录</el-button></el-row>
-        <el-row></el-row>
-        <el-row><el-button>游客登录</el-button></el-row>
-      </form>
+          <el-row></el-row>
+          <el-row></el-row>
+          <el-row>
+            <el-col :span="4"><div>&nbsp;</div></el-col>
+            <el-col :span="16"><el-input placeholder="请输入密码" show-password></el-input></el-col>
+            <el-col :span="4"></el-col>
+          </el-row>
+          <el-row></el-row>
+          <el-row><el-button type="primary">登录</el-button></el-row>
+          <el-row></el-row>
+          <el-row><el-button>游客登录</el-button></el-row>
+        </form>
+        </el-col>
+        <el-col :sm="3" :xs="1"></el-col>
+      </el-row>
     </div>
 </template>
 
@@ -33,7 +38,8 @@ export default {
 
 <style scoped>
 .loginForm{
-  width: 400px;
+  right: 20%;
+  width: 350px;
   height: 400px;
   float: right;
   border: grey solid;
@@ -43,4 +49,15 @@ export default {
 .el-row {
   margin-bottom: 10px;
 }
+  @media screen and (max-width:800px){
+    .loginForm{
+      right: 20%;
+      width: 300px;
+      height: 400px;
+      float: right;
+      border: grey solid;
+      border-radius: 20px;
+      box-shadow: black;
+    }
+  }
 </style>

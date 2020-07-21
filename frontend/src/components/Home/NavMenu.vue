a308a178c12a30909e045684d159347a768acfeb
A-Salty-Fish <1309032417@qq.com>
2020-07-21 15:01:35 +0800

    使用JS路由

--- frontend/src/components/Home/NavMenu.vue
+++ frontend/src/components/Home/NavMenu.vue
@@ -2,7 +2,7 @@
   <div>
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="1">公告</el-menu-item>
-      <el-menu-item index="2">
+      <el-menu-item index="2" @click="GotoLoginPage()">
         <template slot="title"><router-link to="/login">我的实验室</router-link></template>
       </el-menu-item>
       <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">实验室管理</a></el-menu-item>
@@ -13,7 +13,18 @@
 
 <script>
 export default {
-  name: 'NavMenu'
+  name: 'NavMenu',
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    GotoLoginPage () {
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
--- frontend/src/components/Login/TopBar.vue
+++ frontend/src/components/Login/TopBar.vue
@@ -9,7 +9,7 @@ export default {
   name: 'TopBar',
   methods: {
     goBack () {
-      window.history.go(-1)
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
     }
   }
 }

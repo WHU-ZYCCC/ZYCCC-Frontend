facb47140945f88a1ab36f70fb219810f942fd52
A-Salty-Fish <1309032417@qq.com>
2020-08-18 15:26:30 +0800

    使用https://github.com/A-Salty-Fish/LearnVueAdmin替换原来的部分

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,11 @@
 <template>
-  <div>
-    <TopBar></TopBar>
-    <div id="app">
-      <router-view/>
-    </div>
-    <el-divider></el-divider>
-    <BottomAbout></BottomAbout>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
 <script>
-import TopBar from './components/TopBar'
-import BottomAbout from './components/BottomAbout'
 export default {
-  name: 'App',
-  components: {BottomAbout, TopBar}
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>

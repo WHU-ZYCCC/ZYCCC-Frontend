1e2e1efdd7704043fca973717b01be977b5687de
A-Salty-Fish <1309032417@qq.com>
2021-02-11 22:16:24 +0800

    签到表格

--- frontend/src/views/table/index.vue
+++ frontend/src/views/table/index.vue
@@ -1,79 +1,267 @@
 <template>
-  <div class="app-container">
+  <div>
+    <el-divider />
+    <el-row>
+      <el-col :span="3"><br></el-col>
+      <el-col :span="21">
+        <el-form :inline="true" :model="addData">
+          <el-form-item label="姓名">
+            <el-input v-model="addData.name" size="small" placeholder="姓名" />
+          </el-form-item>
+          <el-form-item label="学号">
+            <el-input v-model="addData.school_num" size="small" placeholder="学号" />
+          </el-form-item>
+          <el-form-item label="日期">
+            <el-select v-model="addData.author_level" size="small" placeholder="权限">
+              <el-option v-for="(item, index) in author_levels" :key="index" :label="item" :value="item" />
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" size="mini" @click="Search">搜索</el-button>
+            <el-button type="plain" size="mini" @click="getAllUser">刷新</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
     <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row
+      id="TableTop"
+      :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+      style="width: 100%"
+      @sort-change="SortById"
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column
+        align="center"
+        prop="id"
+        label="学号"
+        sortable="custom"
+      >
         <template slot-scope="scope">
-          {{ scope.$index }}
+          <span style="margin-left: 10px">{{ scope.row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column
+        align="center"
+        label="姓名"
+      >
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          <span style="margin-left: 10px">{{ scope.row.name }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column
+        align="center"
+        label="签到时间"
+      >
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <span style="margin-left: 10px">{{ scope.row.author_level }}</span>
         </template>
       </el-table-column>
     </el-table>
+    <div style="text-align: center;margin-top: 30px;">
+      <el-pagination
+        :current-page="currentPage"
+        :page-size="pageSize"
+        :pager-count="5"
+        layout="total, pager"
+        :total="tableData.length"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import axios from 'axios'
 
 export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
+  name: 'Index',
   data() {
     return {
-      list: null,
-      listLoading: true
+      currentPage: 1,
+      pageSize: 6,
+      tableData: [],
+      allData: [],
+      InvalidInputDialogVisible: false,
+      UpdatedialogVisible: false,
+      author_levels: ['', 1, 2, 3, 4],
+      addData: {
+        'id': -1,
+        'name': '',
+        'headimg_url': '',
+        'faculty': '',
+        'school_num': '',
+        'author_level': 4
+      },
+      upDateData: {
+        'id': -1,
+        'name': '',
+        'headimg_url': '',
+        'faculty': '',
+        'school_num': '',
+        'author_level': 4
+      },
+      sortState: 0
     }
   },
   created() {
-    this.fetchData()
+    var that = this
+    that.getAllUser()
   },
   methods: {
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
+    getAllUser() {
+      console.log()
+      var that = this
+      axios.get(this.$store.state.userHeadUrl + 'GetAllUser').then(function(response) {
+        that.allData = response.data
+        that.tableData = response.data
+        console.log(that.allData)
+      }).catch(function(error) {
+        console.log(error)
       })
+    },
+    deleteUserById(id) {
+      axios.delete(this.$store.state.userHeadUrl + 'DeleteUserById/' + id).then(function(response) {
+        console.log(response.data)
+      }).catch(function(error) {
+        console.log(error)
+      })
+    },
+    addUser() {
+      var that = this
+      axios({
+        method: 'post',
+        url: this.$store.state.userHeadUrl + 'AddUser',
+        data: {
+          'id': that.addData.id,
+          'name': that.addData.name,
+          'headimg_url': that.addData.headimg_url,
+          'faculty': that.addData.faculty,
+          'school_num': that.addData.school_num,
+          'author_level': that.addData.author_level
+        },
+        transformRequest: [
+          function(data) {
+            let ret = ''
+            for (const it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            ret = ret.substring(0, ret.lastIndexOf('&'))
+            return ret
+          }
+        ],
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function(response) {
+        that.getAllUser()
+      })
+    },
+    updateUser() {
+      var that = this
+      axios({
+        method: 'post',
+        url: this.$store.state.userHeadUrl + 'UpdateUser',
+        data: {
+          'id': that.upDateData.id,
+          'name': that.upDateData.name,
+          'headimg_url': that.upDateData.headimg_url,
+          'faculty': that.upDateData.faculty,
+          'school_num': that.upDateData.school_num,
+          'author_level': that.upDateData.author_level
+        },
+        transformRequest: [
+          function(data) {
+            let ret = ''
+            for (const it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            ret = ret.substring(0, ret.lastIndexOf('&'))
+            return ret
+          }
+        ],
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function(response) {
+        that.getAllUser()
+      })
+    },
+    handleCurrentChange(currentPage) {
+      this.currentPage = currentPage
+      location.href = '#TableTop'
+    },
+    IndexOfId(id) {
+      var index = -1
+      for (var i = 0; i < this.tableData.length; i++) {
+        if (this.tableData[i].id === id) {
+          index = i
+        }
+      }
+      return index
+    },
+    handleDelete(id, row) {
+      var index = this.IndexOfId(id)
+      console.log('index:' + index)
+      this.tableData.splice(index, 1)
+      if (this.tableData.length % this.pageSize === 0) {
+        this.currentPage--
+      }
+      console.log('id:' + id)
+      this.deleteUserById(id)
+    },
+    handleEdit(id, row) {
+      this.UpdatedialogVisible = true
+      var index = this.IndexOfId(id)
+      this.upDateData = JSON.parse(JSON.stringify(this.tableData[index]))
+    },
+    commitEdit() {
+      this.updateUser()
+      this.UpdatedialogVisible = false
+    },
+    AddUser() {
+      var that = this
+      if (that.addData.name === '' ||
+        that.addData.author_level === '') {
+        that.InvalidInputDialogVisible = true
+        return
+      }
+      console.log(that.addData)
+      that.addUser()
+    },
+    Search() {
+      var that = this
+      that.currentPage = 1
+      that.tableData = that.allData.filter(item => {
+        return item.name.includes(that.addData.name) &&
+          (item.author_level === that.addData.author_level || that.addData.author_level === that.author_levels[0]) &&
+          item.faculty.includes(that.addData.faculty) &&
+          item.school_num.includes(that.addData.school_num)
+      })
+      console.log(that.tableData)
+    },
+    SortById() {
+      this.sortState = (this.sortState + 1) % 3
+      if (this.sortState === 1) {
+        this.tableData.reverse()
+      } else if (this.sortState === 2) {
+        this.tableData.reverse()
+      }
+      console.log(this.sortState)
     }
   }
 }
 </script>
+
+<style scoped>
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
+</style>

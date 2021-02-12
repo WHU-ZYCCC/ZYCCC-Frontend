c103675f82461004259851a3ab6e26d2f37d2274
A-Salty-Fish <1309032417@qq.com>
2021-02-12 21:44:02 +0800

    获取半径的API

--- frontend/src/api/signin.js
+++ frontend/src/api/signin.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
-const url = 'http://localhost:8080'
+const url = 'http://localhost:8080/signin'
 export function signInAPI() {
   return {
     getToday: function() {
-      return axios.get(url + '/signin/getToday')
+      return axios.get(url + '/getToday')
     },
     getAll: function() {
-      return axios.get(url + '/signin/getAll')
+      return axios.get(url + '/getAll')
+    },
+    getRadius: function() {
+      return axios.get(url + '/radius')
     }
   }
 }

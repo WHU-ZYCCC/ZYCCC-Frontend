7a5d08c49e96254956f93cb1c3817b230cc42fd5
A-Salty-Fish <1309032417@qq.com>
2021-02-12 20:33:51 +0800

    删了些乱七八糟的东西

--- frontend/src/views/table/index.vue
+++ frontend/src/views/table/index.vue
@@ -6,19 +6,19 @@
       <el-col :span="21">
         <el-form :inline="true" :model="addData">
           <el-form-item label="姓名">
-            <el-input v-model="addData.name" size="small" placeholder="姓名" />
+            <el-input size="small" placeholder="姓名" />
           </el-form-item>
           <el-form-item label="学号">
-            <el-input v-model="addData.school_num" size="small" placeholder="学号" />
+            <el-input size="small" placeholder="学号" />
           </el-form-item>
           <el-form-item label="日期">
-            <el-select v-model="addData.author_level" size="small" placeholder="权限">
+            <el-select size="small" placeholder="日期">
               <el-option v-for="(item, index) in author_levels" :key="index" :label="item" :value="item" />
             </el-select>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" size="mini" @click="Search">搜索</el-button>
-            <el-button type="plain" size="mini" @click="getAllUser">刷新</el-button>
+            <el-button type="primary" size="mini" @click="">搜索</el-button>
+            <el-button type="plain" size="mini" @click="">刷新</el-button>
           </el-form-item>
         </el-form>
       </el-col>
@@ -36,7 +36,7 @@
         sortable="custom"
       >
         <template slot-scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.id }}</span>
+          <span style="margin-left: 10px">{{ scope.row.number }}</span>
         </template>
       </el-table-column>
       <el-table-column
@@ -52,7 +52,7 @@
         label="签到时间"
       >
         <template slot-scope="scope">
-          <span style="margin-left: 10px">{{ scope.row.author_level }}</span>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -80,163 +80,22 @@ export default {
       pageSize: 6,
       tableData: [],
       allData: [],
-      InvalidInputDialogVisible: false,
-      UpdatedialogVisible: false,
       author_levels: ['', 1, 2, 3, 4],
-      addData: {
-        'id': -1,
-        'name': '',
-        'headimg_url': '',
-        'faculty': '',
-        'school_num': '',
-        'author_level': 4
-      },
-      upDateData: {
-        'id': -1,
-        'name': '',
-        'headimg_url': '',
-        'faculty': '',
-        'school_num': '',
-        'author_level': 4
-      },
       sortState: 0
     }
   },
   created() {
-    var that = this
-    that.getAllUser()
+    this.tableData.push({
+      name: 'dzy',
+      number: 11111111,
+      date: '2020-12-16'
+    })
+    this.allData = this.tableData
   },
   methods: {
-    getAllUser() {
-      console.log()
-      var that = this
-      axios.get(this.$store.state.userHeadUrl + 'GetAllUser').then(function(response) {
-        that.allData = response.data
-        that.tableData = response.data
-        console.log(that.allData)
-      }).catch(function(error) {
-        console.log(error)
-      })
-    },
-    deleteUserById(id) {
-      axios.delete(this.$store.state.userHeadUrl + 'DeleteUserById/' + id).then(function(response) {
-        console.log(response.data)
-      }).catch(function(error) {
-        console.log(error)
-      })
-    },
-    addUser() {
-      var that = this
-      axios({
-        method: 'post',
-        url: this.$store.state.userHeadUrl + 'AddUser',
-        data: {
-          'id': that.addData.id,
-          'name': that.addData.name,
-          'headimg_url': that.addData.headimg_url,
-          'faculty': that.addData.faculty,
-          'school_num': that.addData.school_num,
-          'author_level': that.addData.author_level
-        },
-        transformRequest: [
-          function(data) {
-            let ret = ''
-            for (const it in data) {
-              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-            }
-            ret = ret.substring(0, ret.lastIndexOf('&'))
-            return ret
-          }
-        ],
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }).then(function(response) {
-        that.getAllUser()
-      })
-    },
-    updateUser() {
-      var that = this
-      axios({
-        method: 'post',
-        url: this.$store.state.userHeadUrl + 'UpdateUser',
-        data: {
-          'id': that.upDateData.id,
-          'name': that.upDateData.name,
-          'headimg_url': that.upDateData.headimg_url,
-          'faculty': that.upDateData.faculty,
-          'school_num': that.upDateData.school_num,
-          'author_level': that.upDateData.author_level
-        },
-        transformRequest: [
-          function(data) {
-            let ret = ''
-            for (const it in data) {
-              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-            }
-            ret = ret.substring(0, ret.lastIndexOf('&'))
-            return ret
-          }
-        ],
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }).then(function(response) {
-        that.getAllUser()
-      })
-    },
     handleCurrentChange(currentPage) {
       this.currentPage = currentPage
       location.href = '#TableTop'
-    },
-    IndexOfId(id) {
-      var index = -1
-      for (var i = 0; i < this.tableData.length; i++) {
-        if (this.tableData[i].id === id) {
-          index = i
-        }
-      }
-      return index
-    },
-    handleDelete(id, row) {
-      var index = this.IndexOfId(id)
-      console.log('index:' + index)
-      this.tableData.splice(index, 1)
-      if (this.tableData.length % this.pageSize === 0) {
-        this.currentPage--
-      }
-      console.log('id:' + id)
-      this.deleteUserById(id)
-    },
-    handleEdit(id, row) {
-      this.UpdatedialogVisible = true
-      var index = this.IndexOfId(id)
-      this.upDateData = JSON.parse(JSON.stringify(this.tableData[index]))
-    },
-    commitEdit() {
-      this.updateUser()
-      this.UpdatedialogVisible = false
-    },
-    AddUser() {
-      var that = this
-      if (that.addData.name === '' ||
-        that.addData.author_level === '') {
-        that.InvalidInputDialogVisible = true
-        return
-      }
-      console.log(that.addData)
-      that.addUser()
-    },
-    Search() {
-      var that = this
-      that.currentPage = 1
-      that.tableData = that.allData.filter(item => {
-        return item.name.includes(that.addData.name) &&
-          (item.author_level === that.addData.author_level || that.addData.author_level === that.author_levels[0]) &&
-          item.faculty.includes(that.addData.faculty) &&
-          item.school_num.includes(that.addData.school_num)
-      })
-      console.log(that.tableData)
     },
     SortById() {
       this.sortState = (this.sortState + 1) % 3
